aa5d5baf77109dfe18e588527037c53881f0bd5e
Sadeq <sadeq1989@gmail.com>
2015-01-09 00:03:34 +0100

    create CheckoutController , add some filters for setter in account and customer model, add new menuelement for admin

---
+++ application/controllers/CheckoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+class CheckoutController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function sendMailAction()
+    {
+        // action body
+    }
+
+}
+
+
+
