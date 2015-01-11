2508f7293dcbaa01d62ee0e36929d579b159a947
Alfred Feldmeyer <alfred.feldmeyer@webtaurus.de>
2015-01-11 17:11:13 +0100

    Validierung von Login in controllern verbessert

--- application/controllers/SmoothiesController.php
+++ application/controllers/SmoothiesController.php
@@ -41,7 +41,7 @@ class SmoothiesController extends Zend_Controller_Action
             $this->view->isAjax=false;
         }
         // Validiere Zugang
-        if (Zend_Auth::getInstance()->hasIdentity()!="admin") {
+        if (Zend_Auth::getInstance()->hasIdentity() && Zend_Auth::getInstance()->getIdentity()->role!=="admin") {
             $this->_helper->flashMessenger
                 ->setNamespace("warning")
                 ->addMessage("Ihre Sitzung ist abgelaufen");
